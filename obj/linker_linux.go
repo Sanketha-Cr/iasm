8940497a8575d7a15892f8d5da7351ad798ac265
Sanketha CR <sankethacr@Sankethas-MacBook-Pro.local>
2025-07-15 16:00:41 +0530

    Added changes to support s390x

--- obj/linker_linux.go
+++ obj/linker_linux.go
@@ -1,8 +1,17 @@
 package obj
 
+import (
+    "os"
+    "os/exec"
+)
+
 // CurrentOS is the OS that iasm is built for.
 const CurrentOS = Linux
 
 func link(dest string, file string) error {
-    panic("iasm: not implemented: link_linux")
+    cmd := exec.Command("ld", "-o", dest, file)
+    cmd.Stdin = nil
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    return cmd.Run()
 }
